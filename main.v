0c1f955f83f9f25fe34819b5a1bc18a9054b39e6
Leah Lundqvist <leah@pigeon.sh>
2021-04-14 17:43:38 +0200

    add public

--- main.v
+++ main.v
@@ -2,7 +2,6 @@
 module main
 
 import vweb
-import rand
 
 const (
 	port = 80
@@ -16,11 +15,17 @@ fn main() {
 	vweb.run<App>(port)
 }
 
+pub fn (mut app App) init_once() {
+	app.handle_static('./public', true)
+}
+
+['/wasm']
 pub fn (mut app App) index() vweb.Result {
 	j := new_compilation_job(app.query['code']) or { return app.text(err.msg) }
 	j.compile()
 	wasm := j.encode() or { return app.text(err.msg) }
 	j.cleanup() or { return app.text(err.msg) }
+	app.add_header('access-control-allow-origin', '*')
 	return app.text(wasm)
 }
 
